311d3f1e68d2965402533181a7624c4bec83e2ad
Fletchy <tom@tomfletcher.tech>
2021-06-10 20:15:20 +0100

    Added required fields

--- Dotnet5.Docker.Templates.PostGresMicroservice/Persistence/Data/TodoItem.cs
+++ Dotnet5.Docker.Templates.PostGresMicroservice/Persistence/Data/TodoItem.cs
@@ -9,14 +9,30 @@
 
 #region usings
 using System;
+using System.ComponentModel.DataAnnotations;
 #endregion
 
 namespace Dotnet5.Docker.Templates.PostGresMicroservice.Persistence.Data
 {
     public class TodoItem
     {
+        /// <summary>
+        /// Get or Sets the Id. 
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Get or Sets the Name. 
+        /// </summary>
+        /// <value>Sets the Name on TodoItem</value>
+        [Required]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Get or Sets the Name. 
+        /// </summary>
+        /// <value>Sets the status on TodoItem</value>
+        [Required]
         public bool IsCompleted { get; set; }
     }
 }
